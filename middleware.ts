ce554faa626c3c63649490b12bca3bf228716b56
muhaimin nur <you@example.com>
2024-08-14 13:27:56 +0800

    install prisma

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,8 @@
-import { clerkMiddleware } from "@clerk/nextjs/server";
+import { authMiddleware, clerkMiddleware } from "@clerk/nextjs/server";
 
-export default clerkMiddleware();
+export default authMiddleware({
+  publicRoutes: ["/api/:path"],
+});
 
 export const config = {
   matcher: [
